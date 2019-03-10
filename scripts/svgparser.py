1c9956cd275d3e73d24c9a49d24713537f78bee9
Jakub Chlanda <j.chlanda@gmail.com>
2019-03-10 08:15:37 +0000

    Output array of coords and intended usage hint.

--- scripts/svgparser.py
+++ scripts/svgparser.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import datetime
 import sys
 import os.path
 import re
@@ -9,7 +10,7 @@ from svgpathtools import svg2paths
 class SvgParser(object):
 
   def __init__(self, inFileName, outFileName):
-    self.counter = 1
+    self.counter = 0
     self.inFileName = inFileName
     self.outFileName = outFileName
     if (self.outFileName):
@@ -20,14 +21,18 @@ class SvgParser(object):
     if (self.outFileName):
       self.outFile.close()
 
-  def output(self, message):
+  def output(self, message, needsNewLine = True):
     if self.outFileName:
       self.outFile.write(message)
-      self.outFile.write("\n")
+      if needsNewLine:
+        self.outFile.write("\n")
     else:
-      print(message)
+      sys.stdout.write(message)
+      if needsNewLine:
+        print("")
 
   def parse_svg(self):
+    self.header()
     shapesCoords = []
     paths, attributes = svg2paths(self.inFileName)
     dAttribute = attributes[0]['d']
@@ -43,36 +48,78 @@ class SvgParser(object):
         shapeCoord.append(points[0])
       shapesCoords.append(shapeCoord)
 
-    for s in shapesCoords:
-      self.process_shape(s)
+    self.process_common()
+    self.process_shapes(shapesCoords)
+    self.footer()
 
-  def process_shape(self, shape):
-    if len(shape) < 2:
-      return
-    shapeName = "shape{0}".format(self.counter)
-    pathName = "path{0}".format(self.counter)
-    self.counter += 1
-    self.output("imageView.layer.addSublayer({0})".format(shapeName))
-    self.output("{0}.opacity = 0.5".format(shapeName))
-    self.output("{0}.lineWidth = 2".format(shapeName))
-    self.output("{0}.lineJoin = CAShapeLayerLineJoin.miter".format(shapeName))
-    self.output(
-        "{0}.strokeColor = UIColor(hue: 0.786, saturation: 0.79, brightness: "
-        "0.53, alpha: 1.0).cgColor".format(shapeName))
-    self.output(
-        "{0}.fillColor = UIColor(hue: 0.786, saturation: 0.15, brightness: "
-        "0.89, alpha: 1.0).cgColor".format(shapeName))
-    firstPoint = shape[0].split(",")
-    self.output("{0}.move(to: CGPoint(x: {1}, y: {2}))".format(
-        pathName, firstPoint[0], firstPoint[1]))
-    for point in shape[2:]:
+  def header(self):
+    now = datetime.datetime.now()
+    self.output("//")
+    self.output("//  ShapesCoords.swift")
+    self.output("//  Block")
+    self.output("//")
+    self.output("//  Created by Jakub on {0}/{1}/{2}.".format(now.day, now.month, now.year))
+    self.output("//  Copyright c 2019 Jakub. All rights reserved.")
+    self.output("//")
+    self.output("//  Auto-generated from svg file, using svgparser.py")
+    self.output("//")
+    self.output("")
+    self.output("class AutoGen {")
+    self.output("  struct FromSVG {")
+    self.output("    static var ShapesCoords: [[(Double, Double)]] = [")
+
+  def footer(self):
+    self.output("  }")
+    self.output("}")
+
+  def process_shapes(self, shapes):
+    for shape in shapes[:-1]:
+      self.output("      [\n       ", False)
+      for i, point in enumerate(shape[:-1]):
+        point = point.split(",")
+        self.output(" ({0}, {1}),".format(point[0], point[1]), False)
+        if 0 == (i + 1) % 4 and i:
+          self.output("\n       ", False)
+      lastPoint = shape[-1].split(",")
+      self.output(" ({0}, {1})\n      ],".format(lastPoint[0], lastPoint[1]))
+    self.output("      [\n        ", False)
+    for i, point in enumerate(shapes[-1][:-1]):
       point = point.split(",")
-      self.output("{0}.addLine(to: CGPoint(x: {1}, y: {2}))".format(
-          pathName, point[0], point[1]))
-    self.output("{0}.close()".format(pathName))
-    self.output("{0}.path = {1}.cgPath".format(shapeName, pathName))
+      self.output("({0}, {1}),".format(point[0], point[1]), False)
+      if 0 == (i + 1) % 4 and i:
+        self.output("\n        ", False)
+    lastPoint = shapes[-1][-1].split(",")
+    self.output("({0}, {1})\n      ]".format(lastPoint[0], lastPoint[1]))
+    self.output("    ]")
+
+  def process_common(self):
+    self.output("//  Intended use:")
+    self.output("//  for ShC in AutoGen.FromSVG.ShapesCoords {")
+    self.output("//    let Shape = CAShapeLayer()")
+    self.output("//    Shapes.append(Shape)")
+    self.output("//    let Path = UIBezierPath()")
+    self.output("//    Path.move(to: CGPoint(x: ShC[0].0, y: ShC[0].1))")
+    self.output("//    for Point in ShC.dropFirst() {")
+    self.output("//      Path.addLine(to: CGPoint(x: Point.0, y: Point.1))")
+    self.output("//    }")
+    self.output("//    Path.close()")
+    self.output("//    Paths.append(Path)")
+    self.output("//  }")
     self.output("")
 
+    self.output("//  for (Sh, P) in zip(Shapes, Paths) {")
+    self.output("//    imageView.layer.addSublayer(Sh)")
+    self.output("//    Sh.opacity = 0.5")
+    self.output("//    Sh.lineWidth = 2")
+    self.output("//    Sh.lineJoin = CAShapeLayerLineJoin.miter")
+    self.output(
+        "//    Sh.strokeColor = UIColor(hue: 0.786, saturation: 0.79, brightness:"
+        "0.53,\n//      alpha: 1.0).cgColor")
+    self.output(
+        "//    Sh.fillColor = UIColor(hue: 0.786, saturation: 0.15, brightness: "
+        "0.89,\n//      alpha: 1.0).cgColor")
+    self.output("//    Sh.path = P.cgPath")
+    self.output("//  }")
 
 def main():
   if len(sys.argv) < 2 and len(sys.argv) < 3:
